4b9e121f039b61a0612e3abe627553cf4b731396
Beaprofessor <professornberlin2122@gmail.com>
2024-07-17 22:35:15 +0530

    Day 1 Completed where I have revised about the variables and the data-Types

---
+++ Day1/01.js
@@ -0,0 +1,92 @@
+// Activity 1 : Variable Declaration 
+
+// Task 1 -> Declare a variable using var , assign it a number and log the value to the console.
+var a = 22
+console.log(a)   
+
+// Task 2 -> Declare a variable using let , assign it a number and log the value to the console. 
+let b = "Chai aur Code"
+console.log(b)
+
+
+// Activity 2 : Constant Declaration 
+
+// Task 3
+const formIsSubmitted = true
+console.log(formIsSubmitted)
+
+
+// Activity 3 : Data Types 
+
+// Task 4
+let numberA = 1
+console.log(typeof numberA)
+
+let stringB = "Chai aur code js challenge"
+console.log(typeof stringB)
+
+let bool = false
+console.log(typeof bool)
+
+let obj = {
+    name: "Chai aur Code",
+    content: "SABSE BEST"
+}
+console.log(typeof obj)
+
+let arr = [1,2,"Sherni",true]
+console.log(typeof arr)
+
+
+// Activity 4 : Reassigning Variables
+
+// TasK 5 
+let value = 11
+value = 22
+
+console.log(value) // So here we can see that we can reassign the value of the declared variable.
+
+
+// Activity 5 : Understanding Const
+
+// TasK 6
+const hero = "jerry"
+hero = 22
+
+console.log(hero) // So here we can see that we can't change the value of the constant declared variable.
+
+
+
+// Feature request : 
+
+// 1.
+
+let number1 = 1
+console.log(number1 , typeof number1)
+
+let string1 = "Chai aur code js challenge"
+console.log(string1,typeof string1)
+
+let bool1 = false
+console.log(bool1,typeof bool1)
+
+let obj1 = {
+    name: "Chai aur Code",
+    content: "SABSE BEST"
+}
+console.log(obj1,typeof obj1)
+
+let arr1 = [1,2,"Sherni",true]
+console.log(arr1,typeof arr1)
+
+
+// 2.
+let value1 = 11
+value1 = 22
+
+console.log(value1)
+
+const hero1 = "jerry"
+hero1 = 22
+
+console.log(hero1)
