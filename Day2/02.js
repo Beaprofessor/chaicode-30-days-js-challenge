652095912c7b0866189cead3c167921c4b7ecd82
Beaprofessor <professornberlin2122@gmail.com>
2024-07-18 21:28:11 +0530

    Day - 2 , - Operators Done

---
+++ Day2/02.js
@@ -0,0 +1,85 @@
+// Acitivity 1 : Arithmetic Operations
+
+let a = 10
+let b = 20
+
+// Task 1
+console.log(a+b) 
+// Task 2
+console.log(b-a)
+// Task 3
+console.log(a*b)
+// Task 4
+console.log(b/a)
+// Task 5
+console.log(a%b)
+
+
+
+
+// Acitivity 2 : Assignment Operators
+
+// Task 6
+a+=b
+console.log(a) 
+
+// Task 7
+b-=a
+console.log(b)
+
+// Acitivity 3 : Comparison Operators
+
+// Task 8
+if(a>b){
+    console.log("True")
+}
+else{
+    console.log("False")
+}
+
+
+// Task 9
+
+if(a>=20){
+    console.log("Its true bruh")
+}
+
+// Task 10
+if(a==10){
+    console.log("Its true bruh")
+}
+else{
+    console.log("Its false bruh")
+}
+
+
+// Acitivity 4 : Logical Operators
+
+// TASK 11
+if(a>=20 && b<= 10){
+    console.log("Its true my friend")
+}
+else{
+    console.log("Its False my friend")
+
+}
+
+// TASK 12
+if(a>=20 || b>= 100){
+    console.log("Its true my bro")
+}
+else{
+    console.log("Its False my bro")
+
+}
+
+// TASK 13
+if(a!= 0){
+    console.log("a ki value 0 nhi hai")
+}
+
+
+// Acitivity 5 : Ternary Operator
+let hero = 22
+const c = hero ? hero>0 : hero<0;
+console.log( " yes hero is " , c)
