9eec372d0fe42e9ff890f90a175da24cf4fafb5c
Beaprofessor <professornberlin2122@gmail.com>
2024-07-25 22:08:05 +0530

    Day13 -> Done with only 1st activity.

---
+++ Day13/script1.js
@@ -0,0 +1,23 @@
+// Day 13 -> Modules
+
+// Activity 1: Creating and Exporting Modules
+
+// Task 1
+// const addition = (a, b) => {
+//   const add = a+b ;
+//   console.log(`${a} + ${b} => ` ,add);
+//   return add;
+// };
+// module.exports = addition
+
+
+
+// Task 2
+
+let person = {
+    name: "Lucky",
+    age: 22,
+    profession: "student",
+}
+
+module.exports = person
---
+++ Day13/script2.js
@@ -0,0 +1,11 @@
+//  Task 1
+// const addition = require('./script1');
+
+// addition(22,2)
+
+//  Task 2
+const person = require('./script1');
+console.log(person.name)
+
+
+// Task 3
