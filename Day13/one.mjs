0f2585dfa42808002389e189de250fba25b59d2a
Beaprofessor <professornberlin2122@gmail.com>
2024-07-29 21:35:31 +0530

    Updated.

--- Day13/one.mjs
+++ Day13/one.mjs
@@ -27,9 +27,64 @@ const person = {
     }
 };
 
-export default person;
+// export default person;
 
 
 //  Activity 2: Named and Default Exports.
 
 //  Task 3
+
+
+export function subtract(a, b) {
+    return a - b;
+}
+
+export function multiply(a, b) {
+    return a * b;
+}
+
+
+export function divide(a, b) {
+    if (b === 0) {
+        console.log('Division by zero is not allowed.');
+    }
+    return a / b;
+}
+
+
+
+// Task 4
+
+export default function greet(name) {
+    return `Hello, ${name}!`;
+}
+
+
+
+// Activity 3: Importing Entire Modules
+
+// Task 5
+
+
+// Constants
+export const PI = 3.14159;
+export const E = 2.71828;
+
+// Functions
+
+export function multiply1(a, b) {
+    return a + b;
+}
+
+export function divide1(a, b) {
+    if (b === 0) {
+        console.log('Division by zero is not allowed.');
+    }
+    return a / b;
+}
+
+
+
+// Activity 4: Using third party Modules
+
+// Task 6
--- Day13/two.mjs
+++ Day13/two.mjs
@@ -1,7 +1,7 @@
 // Task 1 
 
 import {add} from './one.mjs';
-import person from './one.mjs';
+// import person from './one.mjs';
 
 
 const num1 = 7;
@@ -12,17 +12,45 @@ console.log(`The sum of ${num1} and ${num2} is ${add(num1, num2)}`);
 
 // TASK 2 
 
-console.log(person.greet());    // Output: Hello, my name is John Doe.
-console.log(person.getAge());   // Output: I am 30 years old.
+// console.log(person.greet());    // Output: Hello, my name is John Doe.
+// console.log(person.getAge());   // Output: I am 30 years old.
 
 // Modify properties using methods
-person.setName('Jane Smith');
-person.setAge(28);
+// person.setName('Jane Smith');
+// person.setAge(28);
 
-console.log(person.greet());    // Output: Hello, my name is Jane Smith.
-console.log(person.getAge());     // Output: I am 28 years old.
+// console.log(person.greet());    // Output: Hello, my name is Jane Smith.
+// console.log(person.getAge());     // Output: I am 28 years old.
 
 
 
 // Task 3
 
+import { subtract, multiply, divide } from './one.mjs';
+
+const numA = 10;
+const numB = 5;
+
+console.log(`Subtract: ${subtract(numA, numB)}`); 
+console.log(`Multiply: ${multiply(numA, numB)}`); 
+console.log(`Divide: ${divide(numA, numB)}`);
+
+
+
+// Task 4
+
+import greet from './one.mjs';
+
+const name = 'Alice';
+console.log(greet(name)); 
+
+
+// Task 5
+
+import * as one from './one.mjs';
+
+
+console.log(`PI: ${one.PI}`);                 // Output: PI: 3.14159
+console.log(`E: ${one.E}`);                   // Output: E: 2.71828
+console.log(`Multiply: ${one.multiply1(num1, num2)}`); // Output: Multiply: 50
+console.log(`Divide: ${one.divide1(num1, num2)}`); // Output: Divide: 2
